0c6555217c5fd7ab0f7b7df45b67c4c8ca80d3b1
Geneviève Masson <genevieve_masson@hotmail.com>
2017-04-09 19:11:04 -0700

    Add CNAME génération to déploy script

--- deploy.sh
+++ deploy.sh
@@ -25,5 +25,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo "Adding CNAME configuration"
+echo gourmethiking.com >> CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
